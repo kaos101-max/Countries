7b2b3f2774ed99fb3e0c9cca1ceb24eb089dadfc
kaos101-max <watanabedaisuke616@gmail.com>
2026-01-13 19:27:39 +0000

    Second

--- workspace/Country.java
+++ workspace/Country.java
@@ -4,13 +4,25 @@ public class Country
   private String countryName;
   private String capital;
   private String language;
-  private File imageFile;
+  private String imageFile;
   // add constructors
-  public Country(String countryName, String capital, String language,File imageFile){
-
+  public Country(String countryName, String capital, String language,String imageFile){
+        this.countryName = countryName;
+        this.capital = capital;
+        this.language = language;
+        this.imageFile = imageFile;
   }
   // Write accessor/get methods for each instance variable that returns it.
-
+  public String getName(){
+    return countryName;
+    return capital;
+    return language;
+    return imageFile;
+  }
+  public void setName(String name){
+    countryName = name;
+  }
+  }
   // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
  
 
