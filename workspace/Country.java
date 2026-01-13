fc64203ce6901d490104dd92ec18c21a3c1665de
kaos101-max <watanabedaisuke616@gmail.com>
2026-01-13 18:37:25 +0000

    First

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,9 +1,14 @@
 public class Country
 {
   // add private instance variables for the name, capital, language, and image file.
-
+  private String countryName;
+  private String capital;
+  private String language;
+  private File imageFile;
   // add constructors
+  public Country(String countryName, String capital, String language,File imageFile){
 
+  }
   // Write accessor/get methods for each instance variable that returns it.
 
   // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
